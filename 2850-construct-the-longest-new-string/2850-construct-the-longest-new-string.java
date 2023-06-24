fdf08970fcce6f6079699ccca7c45142dd823d98
abhinav u <125759315+abhiii1096@users.noreply.github.com>
2023-06-24 22:29:27 +0530

    Time: 2 ms (100%), Space: 40.6 MB (50%) - LeetHub

---
+++ 2850-construct-the-longest-new-string/2850-construct-the-longest-new-string.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int longestString(int x, int y, int z) {
+        if(x>y)
+        {
+            x=y+1;
+        }
+        else if(x<y)
+        {
+            y=x+1;
+        }
+        return 2*(x+y+z);
+
+    }
+}
