4eb44e7691cf8a15d177a0b3db5cec24521a683c
abhinav u <125759315+abhiii1096@users.noreply.github.com>
2023-09-29 11:21:45 +0530

    Time: 2 ms (62.86%), Space: 55.9 MB (40.74%) - LeetHub

---
+++ 0932-monotonic-array/0932-monotonic-array.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isMonotonic(int[] nums) {
+        boolean increasing = true;
+        boolean decreasing = true;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] < nums[i + 1]) {
+                decreasing = false;
+            } else if (nums[i] > nums[i + 1]) {
+                increasing = false;
+            }
+
+            
+            if (!increasing && !decreasing) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
