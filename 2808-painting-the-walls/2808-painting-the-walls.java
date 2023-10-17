bde1a633a72cf45a7af2e5ba51639e91153424d9
abhinav u <125759315+abhiii1096@users.noreply.github.com>
2023-10-17 23:58:38 +0530

    Time: 31 ms (63.38%), Space: 43.5 MB (88.37%) - LeetHub

--- 2808-painting-the-walls/2808-painting-the-walls.java
+++ 2808-painting-the-walls/2808-painting-the-walls.java
@@ -1,46 +1,47 @@
 class Solution {
     public int paintWalls(int[] cost, int[] time) {
-        final int n = cost.length;
-        final int limit = n << 1;
-        final int m = limit | 1;
+        int n = cost.length;
+        int limit = 2 * n;
+        int m = limit + 1;
         int[][] dp = new int[2][m];
-        
-        
-        for (int[] row : dp) {
-            Arrays.fill(row, -1);
+
+        for (int i = 0; i < 2; i++) {
+            Arrays.fill(dp[i], -1);
         }
-        
+
         dp[0][n] = 0;
         int last = 0;
 
-        for (int i = 0; i < n; ++i) {
-            int now = last ^ 1;
+        for (int i = 0; i < n; i++) {
+            int now = 1 - last;
             Arrays.fill(dp[now], -1);
 
-            for (int j = 0; j <= limit; ++j) {
+            for (int j = 0; j <= limit; j++) {
                 if (dp[last][j] >= 0) {
                     int newTime = Math.min(limit, j + time[i]);
                     int newCost = dp[last][j] + cost[i];
-                    better(dp[now], newTime, newCost);
+                    update(dp[now], newTime, newCost);
 
                     if (j > 0) {
-                        better(dp[now], j - 1, dp[last][j]);
+                        update(dp[now], j - 1, dp[last][j]);
                     }
                 }
             }
 
-            last ^= 1;
+            last = now;
         }
 
-        int[] r = new int[]{Integer.MAX_VALUE}; 
-        for (int i = n; i <= limit; ++i) {
-            better(r, 0, dp[last][i]);
+        int minCost = Integer.MAX_VALUE;
+        for (int i = n; i <= limit; i++) {
+            if (dp[last][i] >= 0) {
+                minCost = Math.min(minCost, dp[last][i]);
+            }
         }
 
-        return r[0] == Integer.MAX_VALUE ? -1 : r[0]; 
+        return minCost == Integer.MAX_VALUE ? -1 : minCost;
     }
 
-    private void better(int[] x, int index, int value) {
+    private void update(int[] x, int index, int value) {
         if (value >= 0 && (x[index] < 0 || x[index] > value)) {
             x[index] = value;
         }
