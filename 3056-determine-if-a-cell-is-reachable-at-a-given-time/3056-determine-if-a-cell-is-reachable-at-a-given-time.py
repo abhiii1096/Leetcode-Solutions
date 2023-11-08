6642cd6199306c0b2482d3de7aca782aa40859c7
abhinav u <125759315+abhiii1096@users.noreply.github.com>
2023-11-08 12:46:38 +0530

    Time: 44 ms (27.22%), Space: 16.2 MB (94.64%) - LeetHub

---
+++ 3056-determine-if-a-cell-is-reachable-at-a-given-time/3056-determine-if-a-cell-is-reachable-at-a-given-time.py
@@ -0,0 +1,5 @@
+class Solution:
+    def isReachableAtTime(self, sx: int, sy: int, fx: int, fy: int, t: int) -> bool:
+        if sx == fx and sy == fy:
+            return t != 1
+        return max(abs(sx - fx), abs(sy - fy)) <= t
